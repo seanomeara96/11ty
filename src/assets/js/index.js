264671c91cbc5c4760719a85456b6b6f40534d41
seanomeara96 <seanom96@gmail.com>
2022-02-23 20:22:59 +0000

    added headline, checklist, and slideshow

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,29 @@
-const body = document.querySelector("body");
-const navBtn = document.querySelector(".nav__button");
-const navLinks = document.querySelector(".nav__links");
-navBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("nav__links--is-open");
-  body.classList.toggle("no-scroll");
-});
+function addNavToggle() {
+  const body = document.querySelector("body");
+  const navBtn = document.querySelector(".nav__button");
+  const navLinks = document.querySelector(".nav__links");
+  navBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("nav__links--is-open");
+    body.classList.toggle("no-scroll");
+  });
+}
+
+function startTestimonialSlides() {
+  const testimonialSection = document.querySelector("#testimonials");
+  const slides = testimonialSection.querySelectorAll(".review");
+  function hideSlides() {
+    slides.forEach((slide) => (slide.style.display = "none"));
+  }
+  hideSlides();
+  let count = 0;
+  function nextSlide() {
+    hideSlides();
+    slides[count].style.display = "flex";
+    count < slides.length - 1 ? count++ : (count = 0);
+    setTimeout(nextSlide, 3000);
+  }
+  nextSlide();
+}
+
+addNavToggle();
+startTestimonialSlides();
